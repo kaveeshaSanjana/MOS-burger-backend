177915c83ac889daa6489d7572de955fd159368c
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-04-07 23:56:19 +0530

    Implementing JWT auth

--- src/main/java/filter/JWTFilter.java
+++ src/main/java/filter/JWTFilter.java
@@ -1,5 +1,7 @@
 package filter;
 
+import edu.icet.ecom.entity.UserEntity;
+import edu.icet.ecom.repository.UserDao;
 import edu.icet.ecom.service.jwt.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -8,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTService jwtService;
+    private final UserDao userDao;
 
     @Override
     protected void  doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
@@ -33,7 +37,21 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         String token = authorization.split(" ")[1];
-        jwtService.getUsername(token);
-        if ()
+        String username = jwtService.getUsername(token);
+        if (username == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        UserEntity user = userDao.findByUsername(username);
+
+        if(username == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(SecurityContextHolder.getContext().getAuthentication() != null){
+            filterChain.doFilter(request,response);
+        }
     }
 }
